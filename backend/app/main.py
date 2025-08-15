958a9e921567208f06c66974bf39196570167005
eunhu0903 <eunhoo0903@naver.com>
2025-08-16 01:24:56 +0900

    feat(main): Auth 라우터 애플리케이션에 등록

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from app.db.models import Base
 from app.db.session import engine
+from app.api import auth
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
+
+app.include_router(auth.router)
