13f1aa64e81b67cdf88fb9459ae850720efb6cd2
eunhu0903 <eunhoo0903@naver.com>
2025-08-25 15:19:43 +0900

    feat(db): PostgreSQL 엔진 및 세션 생성 - DB_TYPE에 따라 MySQL 또는 PostgreSQL 선택 - SQLAlchemy create_engine, sessionmaker 설정 - get_db 함수로 세션 제공

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -3,18 +3,23 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
-engine = create_engine(
-    settings.DATABASE_URL,
-    echo=True,
-    pool_pre_ping=True
-    )
+engine_mysql = create_engine(settings.MYSQL_DATABASE_URL, echo=True, pool_pre_ping=True)
+SessionLocalMySQL = sessionmaker(autocommit = False, autoflush=False, bind=engine_mysql)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine_pg = create_engine(settings.POSTGRES_DATABASE_URL, echo=True, pool_pre_ping=True)
+SessionLocalPG = sessionmaker(autocommit=False, autoflush=False, bind=engine_pg)
 
 Base = declarative_base()
 
-def get_db():
-    db = SessionLocal()
+def get_db_mysql():
+    db = SessionLocalMySQL()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def get_db_pg():
+    db = SessionLocalPG()
     try:
         yield db
     finally:
